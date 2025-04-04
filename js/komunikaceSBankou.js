4478cb94c8f73782ed2b89f06f5e51d5a9a2262d
Vaclav Nekolny <vaclav.nekolny50@gmail.com>
2025-04-04 13:17:34 +0200

    dynamické zmenšení písma v tabulkách

--- js/komunikaceSBankou.js
+++ js/komunikaceSBankou.js
@@ -36,8 +36,8 @@ async function vratCistaData() {
 async function vytvorSelectNabidkuMen() {
   vyberMenySelect = document.getElementById("vyberMenySelect");
 
-  let cistaData = await vratCistaData(); // čtení dat z API
-  // let cistaData = testovaciData;      // čtení dat z testovací proměnné
+  // let cistaData = await vratCistaData(); // čtení dat z API
+  let cistaData = testovaciData; // čtení dat z testovací proměnné
 
   for (let i = 1; i < cistaData.length; i++) {
     let selectMena = document.createElement("option");
@@ -51,28 +51,12 @@ async function vytvorSelectNabidkuMen() {
 
 // TESTOVACÍ DATA -> slouží k tomu, aby se nevyčerpal limit dotazů na API při testování
 testovaciData = [
-  { zkratka: "PHP", nazev: "Filipínské peso", kurz: 0.40389 },
-  { zkratka: "HKD", nazev: "Hongkongský dolar", kurz: 2.969 },
-  { zkratka: "INR", nazev: "Indická rupie", kurz: 0.27025 },
   { zkratka: "IDR", nazev: "Indonéská rupie", kurz: 0.001395 },
-  { zkratka: "ISK", nazev: "Islandská koruna", kurz: 0.17366 },
   { zkratka: "ILS", nazev: "Izraelský šekel", kurz: 6.244 },
   { zkratka: "JPY", nazev: "Japonský jen", kurz: 0.15477 },
-  { zkratka: "ZAR", nazev: "Jihoafrický rand", kurz: 1.241 },
-  { zkratka: "KRW", nazev: "Jihokorejský won", kurz: 0.0158 },
   { zkratka: "CAD", nazev: "Kanadský dolar", kurz: 16.127 },
   { zkratka: "HUF", nazev: "Maďarský forint", kurz: 0.06223 },
-  { zkratka: "MYR", nazev: "Malajsijský ringgit", kurz: 5.188 },
   { zkratka: "MXN", nazev: "Mexické peso", kurz: 1.135 },
-  { zkratka: "XDR", nazev: "MMF", kurz: 30.668 },
   { zkratka: "NOK", nazev: "Norská koruna", kurz: 2.212 },
-  { zkratka: "NZD", nazev: "Novozélandský dolar", kurz: 13.264 },
-  { zkratka: "PLN", nazev: "Polský zlotý", kurz: 5.972 },
-  { zkratka: "RON", nazev: "Rumunský lei", kurz: 5.014 },
-  { zkratka: "SGD", nazev: "Singapurský dolar", kurz: 17.208 },
-  { zkratka: "SEK", nazev: "Švédská koruna", kurz: 2.315 },
-  { zkratka: "CHF", nazev: "Švýcarský frank", kurz: 26.147 },
-  { zkratka: "THB", nazev: "Thajský baht", kurz: 0.67616 },
-  { zkratka: "TRY", nazev: "Turecká lira", kurz: 0.60946 },
   { zkratka: "USD", nazev: "Americký dolar", kurz: 23.106 },
 ];
